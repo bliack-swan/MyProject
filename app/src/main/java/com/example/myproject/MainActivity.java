9faa00911ad1d6c14bacffde15d6ef5a11087f92
bliack-swan <61846791+bliack-swan@users.noreply.github.com>
2020-08-03 10:42:48 +0300

    Created button <Push me>. Click on this show you a simple text: "It's work!"

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView its_work;
+    TextView github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView github = findViewById(R.id.link_git_hub);
+        github = findViewById(R.id.link_git_hub);
+        its_work = findViewById(R.id.its_work);
         github.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+    public void onPushMe(View view){
+        its_work.setTextColor(getResources().getColor(R.color.my_black));
     }
 }
